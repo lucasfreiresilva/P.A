a46ee3b00ef33525da92b03e256863500ed7128f
lucasfreiresilva <lucas.silva4314@etec.sp.gov.br>
2023-09-14 20:42:45 -0300

    Atividades Aula 14/09/2023

---
+++ PA/Aula_14_09_2023/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Aula_14_09_2023
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal n1, n2, n3, n4, media;
+
+            Console.Write("informe a Nota 01: ");
+            n1 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Nota 02: ");
+            n2 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Nota 03: ");
+            n3 = decimal.Parse(Console.ReadLine());
+
+            Console.Write("Informe a Nota 04: ");
+            n4 = decimal.Parse(Console.ReadLine());
+
+            media = (n1 + n2 + n3 + n4) / 4;
+
+            Console.WriteLine("A media Final é: " + media);
+
+
+
+
+
+        }
+    }
+}
